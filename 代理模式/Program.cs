deb6171439a51e0348da1b24719e5fc79fc55022
llibetter <915133364@qq.com>
2020-09-24 21:20:53 +0800

    second commit

---
+++ 代理模式/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace 代理模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy=new Proxy();
+            proxy.Request();
+            Console.Read();
+        }
+    }
+
+    
+    public interface Subject
+    {
+        void Request();
+    }
+    public class ConcreteSubject : Subject
+    {
+        public void Request()
+        {
+            Console.WriteLine("我是真实主题，我正在提供服务");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        private ConcreteSubject realSubject;
+        public void Request()
+        {
+            if (realSubject == null)
+            {
+                realSubject = new ConcreteSubject();
+            }
+            preRequest();
+            realSubject.Request();
+            postRequest();
+        }
+        public void preRequest()
+        {
+            Console.WriteLine("访问真实主题之前的预处理。");
+        }
+        public void postRequest()
+        {
+            Console.WriteLine("访问真实主题之后的后续处理。");
+        }
+    }
+
+
+}
