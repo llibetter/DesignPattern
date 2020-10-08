b8c483579e6473818d898d0b2838918558f07f0c
llibetter <915133364@qq.com>
2020-10-08 20:05:12 +0800

    修改变量命名

--- 解释器模式/Program.cs
+++ 解释器模式/Program.cs
@@ -7,8 +7,8 @@ namespace 解释器模式
         static void Main(string[] args)
         {
             Context bus = new Context();
-            bus.freeRide("韶关的老人");
-            bus.freeRide("韶关的年轻人");
+            bus.freeRide("北京的老人");
+            bus.freeRide("北京的壮年");
             bus.freeRide("广州的妇女");
             bus.freeRide("广州的儿童");
             bus.freeRide("山东的儿童");
@@ -31,19 +31,15 @@ namespace 解释器模式
         }
         public bool interpret(String info)
         {
-            if (set.Contains(info))
-            {
-                return true;
-            }
-            return false;
+            return set.Contains(info);
         }
     }
     //非终结符表达式类
-    class AndExpression : Expression
+    class NonTerminalExpression : Expression
     {
         private Expression city = null;
         private Expression person = null;
-        public AndExpression(Expression city, Expression person)
+        public NonTerminalExpression(Expression city, Expression person)
         {
             this.city = city;
             this.person = person;
@@ -58,27 +54,27 @@ namespace 解释器模式
     //环境类
     /*文法规则
     <expression> ::= <city>的<person>
-    <city> ::= 韶关|广州
+    <city> ::= 北京|广州
     <person> ::= 老人|妇女|儿童
     */
     class Context
     {
-        private String[] citys = { "韶关", "广州" };
+        private String[] citys = { "北京", "广州" };
         private String[] persons = { "老人", "妇女", "儿童" };
         private Expression cityPerson;
         public Context()
         {
             Expression city = new TerminalExpression(citys);
             Expression person = new TerminalExpression(persons);
-            cityPerson = new AndExpression(city, person);
+            cityPerson = new NonTerminalExpression(city, person);
         }
         public void freeRide(String info)
         {
             bool ok = cityPerson.interpret(info);
             if (ok)
-                Console.WriteLine("您是" + info + "，您本次乘车免费！");
+                Console.WriteLine($"{info},乘车免费");
             else
-                Console.WriteLine(info + "，您不是免费人员，本次乘车扣费2元！");
+                Console.WriteLine($"{info},乘车2元");
         }
     }
 }
